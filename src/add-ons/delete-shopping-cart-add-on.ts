e670c8e715e9733bfeba8c621364b83ec28b58ca
Florian Luther <Florian.Luther@live.de>
2023-04-23 17:42:29 +0200

    feat: make delete cart items button TypeScript-able

---
+++ src/add-ons/delete-shopping-cart-add-on.ts
@@ -0,0 +1,112 @@
+"use strict";
+
+type Storage = {
+    cart: Cart;
+};
+
+type Cart = {
+    items: Item[];
+};
+
+type Item = {
+    item_id: string;
+    product_sku: string;
+};
+
+export function initialize() {
+    addButtonStyle();
+
+    document.addEventListener("onCustomInitialize", function () {
+        renderDeleteButton();
+    });
+}
+
+function addButtonStyle() {
+    const css =
+        ".btn-warn { background-color: #e52d00; } .btn-warn:hover { background-color: #a82b01; }";
+    const style = document.createElement("style");
+
+    style.appendChild(document.createTextNode(css));
+
+    document.getElementsByTagName("head")[0].appendChild(style);
+}
+
+function renderDeleteButton() {
+    const deleteButton = document.createElement("button");
+    deleteButton.onclick = function () {
+        deleteCartItems();
+    };
+    deleteButton.innerText = "Warenkorb leeren";
+    deleteButton.className = "btn btn-warn";
+    deleteButton.style.display = "inline";
+    deleteButton.style.margin = "6px";
+
+    const footer = document.getElementsByClassName("sticky-footer")[0];
+    footer.appendChild(deleteButton);
+}
+
+function deleteCartItems() {
+    const ids = getCartItemIds();
+
+    for (const id of ids) {
+        deleteCartItem(id);
+    }
+
+    console.log(`Removed all ${ids.length} items from the shopping cart.`);
+}
+
+async function deleteCartItem(itemId: string): Promise<void> {
+    const element = document.querySelector(
+        "input[name=form_key]"
+    ) as HTMLInputElement;
+
+    if (!element) {
+        return;
+    }
+
+    const formKey = element.value;
+
+    const response = await fetch("/checkout/sidebar/removeItem/?ajax=1", {
+        headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        body: "form_key=" + formKey + "&item_id=" + itemId,
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+    });
+
+    if (response.ok) {
+        toggleCart();
+    }
+
+    if (response.redirected) {
+        window.location.href = response.url;
+    } else if (response.ok) {
+        const reloadCustomerDataEvent = new CustomEvent(
+            "reload-customer-section-data"
+        );
+        window.dispatchEvent(reloadCustomerDataEvent);
+    }
+}
+
+function getCartItemIds(): string[] {
+    const storageAsText = localStorage.getItem("mage-cache-storage");
+
+    if (!storageAsText) {
+        return [];
+    }
+
+    const storage: Storage = JSON.parse(storageAsText);
+    const cartItems = storage.cart.items;
+
+    const ids = cartItems.map((c) => c.item_id);
+
+    return ids;
+}
+
+function toggleCart() {
+    document
+        .querySelector("body")!
+        .classList.remove("overflow-hidden", "md:overflow-auto");
+}
